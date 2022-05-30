3ca229077dd88b3b1b45cd083cfe2d8b6abfa0ac
Amin <amin.haddou@frg.wwschool.de>
2022-05-30 14:22:18 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Discord-HTTP-Interactions
+An example for a Discord Application that receives Interactions via an outgoing Webhook made using Typescript and Express.\
+Check out the [The Developer Portal](https://discord.com/developers/docs/interactions/receiving-and-responding#security-and-authorization) to read more about this.
---
+++ src/index.ts
@@ -0,0 +1,51 @@
+import express from "express"
+import nacl from "tweetnacl"
+import dotenv from "dotenv"
+
+console.log("Hello Discord")
+
+dotenv.config()
+const PUBLIC_KEY = process.env.PUBLIC_KEY
+if(!PUBLIC_KEY) {
+    console.error("No public key")
+    process.exit(-1)
+}
+
+const app = express()
+
+app.use(express.json())
+
+app.post("/", (req, res) => {
+    const signature = req.headers['x-signature-ed25519'] as string;
+    const timestamp = req.headers['x-signature-timestamp'];
+
+    let isVerified;
+    let error;
+    try {
+        isVerified = nacl.sign.detached.verify(
+            Buffer.from(timestamp + JSON.stringify(req.body)),
+            Buffer.from(signature, 'hex'),
+            Buffer.from(PUBLIC_KEY, 'hex')
+        );
+    } catch (e) {
+        error = e;
+    }
+
+    if (error || !isVerified) {
+        console.log("Unauthorized Request")
+        res.status(401).send("Unauthorized")
+        return
+    }
+
+    if(req.body.type==1) {
+        console.log("Received Ping")
+        res.status(200).send({
+            "type": 1
+        })
+    }
+
+})
+
+app.listen(3000, () => {
+    console.log("Listening on port 8080")
+})
