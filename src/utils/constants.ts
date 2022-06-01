ace5c31acc76b1e32e4f4420aa81e251e2711675
Amin <amin.haddou@frg.wwschool.de>
2022-06-01 19:04:26 +0200

    make global option case insensitive

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,7 +4,7 @@ export const PUBLIC_KEY = process.env.PUBLIC_KEY as string;
 export const APPLICATION_ID = process.env.APPLICATION_ID as string;
 export const BOT_TOKEN = process.env.BOT_TOKEN as string;
 export const GUILD_ID = process.env.GUILD_ID as string;
-export const GLOBAL = process.env.GLOBAL === 'true';
+export const GLOBAL = (process.env.GLOBAL as string).toLowerCase() === 'true';
 
 export const PORT = process.env.PORT as string;
 
